305c2756e5bd63000f8b12c16274613ae235fb6b
panchoo-k <panchoo_k@silpakorn.edu>
2026-07-18 14:15:57 +0700

    Test

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int n = 0;
+    float timer = 0f;
+
+    void Awake()
+    {
+        Debug.Log("Awake!");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        n++;
+
+        //Debug.Log(Time.deltaTime);
+
+        if (timer >= 1f)
+        {
+            Debug.Log(n);
+            timer = 0f;
+            n = 0;
+        }
+    }
+}
